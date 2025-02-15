540350359f5427aba40b2a90f709a80f8416d620
unfold <studiovogue01@gmail.com>
2025-02-15 15:58:35 +0530

    dashboard

--- src/app/my_account/my-account-dashboard/my-account-dashboard.component.ts
+++ src/app/my_account/my-account-dashboard/my-account-dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MyAccountHeaderComponent } from '../my-account-header/my-account-header.component';
+import { User } from '../../../model/user.model';
+import { UserAuthService } from '../../_service/user-auth.service';
+import { UserService } from '../../_service/user.service';
 
 @Component({
   selector: 'app-my-account-dashboard',
@@ -8,6 +11,23 @@ import { MyAccountHeaderComponent } from '../my-account-header/my-account-header
   templateUrl: './my-account-dashboard.component.html',
   styleUrl: './my-account-dashboard.component.scss'
 })
-export class MyAccountDashboardComponent {
+export class MyAccountDashboardComponent implements OnInit {
+  user!: User;
 
+  constructor(
+    private userService: UserService,
+    private userAuthService: UserAuthService,
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.userAuthService.getUserId(); // Replace with actual logged-in user ID
+    this.userService.getUserDetails(userId  ).subscribe(
+      (data) => {
+        this.user = data;
+      },
+      (error) => {
+        console.error('Error fetching user data', error);
+      }
+    );
+  }
 }
